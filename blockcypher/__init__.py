d207d16e711b34ef9fed9f3091f257d552b05f50
Michael Flaxman <mflaxman@gmail.com>
2014-11-29 22:56:11 -0800

    add support for pushtx

--- blockcypher/__init__.py
+++ blockcypher/__init__.py
@@ -32,6 +32,7 @@ from .api import get_block_hash
 from .api import get_block_height
 from .api import get_broadcast_transactions
 from .api import get_broadcast_transaction_hashes
+from .api import pushtx
 from .api import get_forwarding_address
 from .api import list_forwarding_addresses
 from .api import delete_forwarding_address
--- blockcypher/api.py
+++ blockcypher/api.py
@@ -590,6 +590,40 @@ def delete_forwarding_address(payment_id, coin_symbol='btc'):
     return r.text
 
 
+def get_pushtx_url(coin_symbol='btc'):
+    """
+    Used for pushing hexadecimal transactions to the network
+    """
+    assert is_valid_coin_symbol(coin_symbol)
+    return 'https://api.blockcypher.com/v1/%s/%s/txs/push' % (
+            COIN_SYMBOL_MAPPINGS[coin_symbol]['blockcypher_code'],
+            COIN_SYMBOL_MAPPINGS[coin_symbol]['blockcypher_network'],
+            )
+
+
+def pushtx(tx_hex, coin_symbol='btc', api_key=None):
+    '''
+    Takes a signed transaction hex (and coin_symbol) and broadcasts it to the bitcoin network.
+    '''
+
+    # This check appears to work for other blockchains
+    # TODO: verify and/or improve
+    assert is_valid_coin_symbol(coin_symbol)
+
+    url = get_pushtx_url(coin_symbol=coin_symbol)
+
+    if DEBUG_MODE:
+        print(url)
+
+    params = {'tx': tx_hex}
+    if api_key:
+        params['token'] = api_key
+
+    r = requests.post(url, data=json.dumps(params), verify=True, timeout=20)
+
+    return json.loads(r.text)
+
+
 def get_websocket_url(coin_symbol):
 
     assert is_valid_coin_symbol(coin_symbol)
