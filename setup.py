811cb4caf155f00137565f73727b081e6c29441e
Michael Flaxman <mflaxman@gmail.com>
2015-07-26 14:43:59 -0400

    new version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
     from distutils.core import setup
 
 setup(name='blockcypher',
-      version='1.0.5',
+      version='1.0.6',
       description='BlockCypher Python Library',
       author='Michael Flaxman',
       author_email='mflaxman+blockcypher@gmail.com',
