17f2f9e920f4d7834914b95c7e3e52092f69e8cf
Michael Flaxman <mflaxman@gmail.com>
2016-01-20 15:15:39 -0500

    bump version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
     from distutils.core import setup
 
 setup(name='blockcypher',
-      version='1.0.57',
+      version='1.0.58',
       description='BlockCypher Python Library',
       author='Michael Flaxman',
       author_email='mflaxman+blockcypher@gmail.com',
