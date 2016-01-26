e72a04b1aa778bb215946787d0bc74cc7ad92598
Muneeb Ali <muneeb@ali.vc>
2016-01-25 22:40:33 -0500

    Switched to bitcoin 1.1.39, bumped version

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ except ImportError:
     from distutils.core import setup
 
 setup(name='blockcypher',
-      version='1.0.59',
+      version='1.0.60',
       description='BlockCypher Python Library',
       author='Michael Flaxman',
       author_email='mflaxman+blockcypher@gmail.com',
       url='https://github.com/blockcypher/blockcypher-python/',
       install_requires=[
-          'requests>=2.4.3',
-          'python-dateutil>=2.2',
-          'bitcoin>=1.1.38',
+          'requests==2.4.3',
+          'python-dateutil==2.2',
+          'bitcoin==1.1.39',
           ],
       packages=['blockcypher'],
       include_package_data=True,
