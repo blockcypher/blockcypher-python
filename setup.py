967548f239a129ea44e00454267aec4ad0145cf5
Matthieu Riou <matthieu@blockcypher.com>
2017-12-17 16:34:40 -0800

    Bump version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
     from distutils.core import setup
 
 setup(name='blockcypher',
-      version='1.0.72',
+      version='1.0.73',
       description='BlockCypher Python Library',
       author='Michael Flaxman',
       author_email='mflaxman+blockcypher@gmail.com',
