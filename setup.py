4e906391667f5d9569059cf107532b39f5624284
Quentin Le Sceller <q.lesceller@gmail.com>
2020-01-07 12:34:41 -0500

    Fix wrong url path on Windows (#80)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
     from distutils.core import setup
 
 setup(name='blockcypher',
-      version='1.0.79',
+      version='1.0.80',
       description='BlockCypher Python Library',
       author='Michael Flaxman',
       author_email='mflaxman+blockcypher@gmail.com',
