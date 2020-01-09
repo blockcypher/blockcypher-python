1599a48d25a03728a5bee7282519e57d296b2db8
Quentin Le Sceller <q.lesceller@gmail.com>
2020-01-09 11:54:18 -0500

    Add readme.md on Pypi

--- setup.py
+++ setup.py
@@ -4,7 +4,15 @@ try:
 except ImportError:
     from distutils.core import setup
 
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(name='blockcypher',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       version='1.0.80',
       description='BlockCypher Python Library',
       author='Michael Flaxman',
